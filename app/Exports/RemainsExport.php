4cc5e18d0865de9499e5ee2fea2a65540e283af7
andrejcvekelis <a.cvekelis@gmail.com>
2021-05-06 13:00:48 +0300

    fix

---
+++ app/Exports/RemainsExport.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Exports;
+
+use App\Dal\Entities\Remains;
+use Maatwebsite\Excel\Concerns\FromCollection;
+
+class RemainsExport implements FromCollection
+{
+    /**
+    * @return \Illuminate\Support\Collection
+    */
+    public function collection()
+    {
+        return Remains::all();
+    }
+}
